2eb0a00e06c640ed0b67de9be5b33e43cc04cf13
“edo” <edouardlicn@gmail.com>
2026-04-29 13:03:32 +0800

    update csrf

--- customer_cn/module.py
+++ customer_cn/module.py
@@ -18,7 +18,7 @@ MODULE_INFO = {
     'id': 'customer_cn',
     'name': '客户信息（国内）',
     'type': 'node',
-    'version': '1.1.5',
+    'version': '1.1.6',
     'author': 'edouardlicn',
     'description': '国内客户信息管理模块',
     'models': ['CustomerCnFields'],
